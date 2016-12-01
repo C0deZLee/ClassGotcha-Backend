c721cb3a92c1509f1f06d2036e6ab94594e653b3
C0deZ <c0dezlee@gmail.com>
2016-11-30 19:36:43 -0500

    remove django_socket_io

--- classgotcha/apps/chat/views.py
+++ classgotcha/apps/chat/views.py
@@ -1,14 +1,8 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import get_object_or_404, render, redirect
-from django_socketio import broadcast, broadcast_channel, NoSocket
-
-from classgotcha.apps.chat.models import Room
-import random
-import string
-from django.db import transaction
-from django.shortcuts import render, redirect
 from haikunator import Haikunator
 
+from classgotcha.apps.chat.models import Room
+from django.db import transaction
+from django.shortcuts import render, redirect
 
 
 def rooms(request, template="rooms.html"):
@@ -58,21 +52,3 @@ def chat_room(request, label):
         'room': room,
         'messages': messages,
     })
-
-##############################
-@user_passes_test(lambda user: user.is_staff)
-def system_message(request, template="system_message.html"):
-    context = {"rooms": Room.objects.all()}
-    if request.method == "POST":
-        room = request.POST["room"]
-        data = {"action": "system", "message": request.POST["message"]}
-        try:
-            if room:
-                broadcast_channel(data, channel="room-" + room)
-            else:
-                broadcast(data)
-        except NoSocket, e:
-            context["message"] = e
-        else:
-            context["message"] = "Message sent"
-    return render(request, template, context)
