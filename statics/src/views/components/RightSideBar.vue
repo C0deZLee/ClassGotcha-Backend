7838ea7926c1a60ea858986bce4e03baa8275e3c
C0deZ <c0dezlee@gmail.com>
2017-02-09 02:23:44 -0500

    front end: show friend list

--- statics/src/views/components/RightSideBar.vue
+++ statics/src/views/components/RightSideBar.vue
@@ -15,7 +15,6 @@
             <div id="tab-1" class="tab-pane active">
                <div class="sidebar-title">
                   <h3> <i class="fa fa-comments-o"></i> Recent Chats</h3>
-                  <small><i class="fa fa-tim"></i> You have 0 new message.</small>
                </div>
                <div>
                   <div v-for="chatroom in chatrooms" class="sidebar-message" >
@@ -43,98 +42,33 @@
             </div>
             <div id="tab-2" class="tab-pane">
                <div class="sidebar-title">
-                  <h3> <i class="fa fa-cube"></i> Latest projects</h3>
-                  <small><i class="fa fa-tim"></i> You have 14 projects. 10 not completed.</small>
+                  <h3> <i class="fa fa-users"></i> Friend List</h3>
                </div>
-               <ul class="sidebar-list">
-                  <li>
-                     <a href="#">
-                        <div class="small pull-right m-t-xs">9 hours ago</div>
-                        <h4>Business valuation</h4>
-                        It is a long established fact that a reader will be distracted.
-                        <div class="small">Completion with: 22%</div>
-                        <div class="progress progress-mini">
-                           <div style="width: 22%;" class="progress-bar progress-bar-warning"></div>
+                            <div>
+                  <div v-for="friend in friends" class="sidebar-message" >
+                     <!--<router-link>-->
+                         <a>
+                        <div class="pull-left text-center">
+                           <img  class="img-circle message-avatar" >
+                           <div class="m-t-xs">
+                              <i class="fa fa-star text-warning"></i>
+                              <i class="fa fa-star text-warning"></i>
+                           </div>
                         </div>
-                        <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <div class="small pull-right m-t-xs">9 hours ago</div>
-                        <h4>Contract with Company </h4>
-                        Many desktop publishing packages and web page editors.
-                        <div class="small">Completion with: 48%</div>
-                        <div class="progress progress-mini">
-                           <div style="width: 48%;" class="progress-bar"></div>
+                        <div class="media-body">
+                           {{friend.full_name}}
+                           <br>
+                           <!--<div v-if="chatroom.latest_message">
+                              <strong>{{chatroom.latest_message.full_name}}</strong>: {{chatroom.latest_message.message}}
+                              <br>
+                              <br>
+                              <small class="text-muted">{{chatroom.latest_message.created}}</small>
+                           </div>-->
                         </div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <div class="small pull-right m-t-xs">9 hours ago</div>
-                        <h4>Meeting</h4>
-                        By the readable content of a page when looking at its layout.
-                        <div class="small">Completion with: 14%</div>
-                        <div class="progress progress-mini">
-                           <div style="width: 14%;" class="progress-bar progress-bar-info"></div>
-                        </div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <span class="label label-primary pull-right">NEW</span>
-                        <h4>The generated</h4>
-                        There are many variations of passages of Lorem Ipsum available.
-                        <div class="small">Completion with: 22%</div>
-                        <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <div class="small pull-right m-t-xs">9 hours ago</div>
-                        <h4>Business valuation</h4>
-                        It is a long established fact that a reader will be distracted.
-                        <div class="small">Completion with: 22%</div>
-                        <div class="progress progress-mini">
-                           <div style="width: 22%;" class="progress-bar progress-bar-warning"></div>
-                        </div>
-                        <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <div class="small pull-right m-t-xs">9 hours ago</div>
-                        <h4>Contract with Company </h4>
-                        Many desktop publishing packages and web page editors.
-                        <div class="small">Completion with: 48%</div>
-                        <div class="progress progress-mini">
-                           <div style="width: 48%;" class="progress-bar"></div>
-                        </div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <div class="small pull-right m-t-xs">9 hours ago</div>
-                        <h4>Meeting</h4>
-                        By the readable content of a page when looking at its layout.
-                        <div class="small">Completion with: 14%</div>
-                        <div class="progress progress-mini">
-                           <div style="width: 14%;" class="progress-bar progress-bar-info"></div>
-                        </div>
-                     </a>
-                  </li>
-                  <li>
-                     <a href="#">
-                        <span class="label label-primary pull-right">NEW</span>
-                        <h4>The generated</h4>
-                        <!--<div class="small pull-right m-t-xs">9 hours ago</div>-->
-                        There are many variations of passages of Lorem Ipsum available.
-                        <div class="small">Completion with: 22%</div>
-                        <div class="small text-muted m-t-xs">Project end: 4:00 pm - 12.06.2014</div>
-                     </a>
-                  </li>
-               </ul>
+                        </a>
+                     <!--</router-link>-->
+                  </div>
+               </div>
             </div>
          </div>
       </div>
