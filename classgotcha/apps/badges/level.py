2eaa80b26453d34dea5dcd626e588a602f57025a
C0deZ <c0dezlee@gmail.com>
2017-05-11 21:35:28 -0400

    Add auto level up
    Formula: (level-1) -> level: int(2.5**(account.level-1)*10)*10

--- classgotcha/apps/badges/level.py
+++ classgotcha/apps/badges/level.py
@@ -8,8 +8,18 @@ def init_actions():
 	pass
 
 
+def level_up(account, exp):
+	# this is how we calculate exp for each level
+	level_up_exp = int(2.5**(account.level-1)*10)*10
+	account.exp += exp
+	if account.exp > level_up_exp:
+		account.level += 1
+	account.save()
+	trigger_action(account, "levelup")
+	return
+
+
 def trigger_action(account, action_name):
 	action = get_object_or_404(Action.objects.all(), name=action_name)
-	account.exp = account.exp + action.exp
-	# TODO: how to auto level-up ?
+	level_up(account, action.exp)
 	pass
