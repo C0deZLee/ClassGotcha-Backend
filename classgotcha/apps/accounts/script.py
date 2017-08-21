03b9a9aa56394dbd5c97a20fba6e80aca3da9aa2
wusimo <szw184@psu.edu>
2017-08-20 23:24:36 -0400

    sample add recommendation task

--- classgotcha/apps/accounts/script.py
+++ classgotcha/apps/accounts/script.py
@@ -5,16 +5,47 @@ from django.db import models
 from ..accounts.models import Account, Group
 from ..classrooms.models import Classroom
 from ..tasks.models import Task
+from datetime import datetime
 # import Intervals 
 
 # TODO download the intervals package
+
+
+
 def generate_recommendations_for_quiz(date):
 
+	# review for the quiz one week prior and 2 days prior
+
+
 	
 
-def generate_recommendations_for_homework(date):
+def generate_recommendations_for_homework(account,task): # in this case the end time of the task is the due date
 	
+	due_date = task.end
+	work_date = due_date- datetime.timedelta(days = 2) # in this default setting do the homework 2 days prior to the due date
+	# get the user's free time
+	free_intervals = get_user_free_intervals(account = account,date = work_date)
 
+	task_add = False
+	
+	for interval in free_intervals:
+		# find approriate time to do things
+		if (interval[0]>8)and (interval[1]< 20)and (interval[1]-interval[0]>1)
+
+		# generate a new task to do the homework
+
+		new_task = Task(task_name = 'do homework for ' task.task_name, category = 'Homework',
+						start = datetime.datetime(work_date.year, work_date.month, work_date.day, int(interval[0]), int(60*(interval[0]-int(interval[0])))), 
+						end = datetime.datetime(work_date.year, work_date.month, work_date.day, int(interval[1]), int(60*(interval[1]-int(interval[1])))),
+						classroom = task.classroom, creator = account)
+
+
+		new_task.save()
+		task_add = True
+
+		break
+
+	return task_add
 
 weekday_dict = {1:'Mo',2:'Tu',3:'We',4:'Th',5:'Fi'}
 
@@ -27,7 +58,7 @@ def get_user_free_intervals(account,date):
 	all_class_intervals = []
 
 	for classe in all_class:
-		all_class_intervals.append([classe.start,classe.end])
+		all_class_intervals.append([float(classe.start.hour+float(classe.start.minute)/60),float(classe.end.hour+float(classe.end.minute)/60)])
 
 	all_customized_tasks = account.tasks.filter(category = 'Other',repeat = True ,repeat__contains = weekday)
 
@@ -35,14 +66,14 @@ def get_user_free_intervals(account,date):
 
 		for customized_task in all_customized_tasks:
 
-			all_class_intervals.append([customized_task.start,customized_task.end])
+			all_class_intervals.append([float(customized_task.start.hour+float(customized_task.start.minute)/60),float(customized_task.end.hour+float(customized_task.end.minute)/60)])
 
 	all_non_repeat_tasks = account.tasks.filter(category = 'Other',repeat = False, start__date=date)
 
 	if all_non_repeat_tasks:
 		for customized_task in all_non_repeat_tasks:
 
-			all_class_intervals.append([all_non_repeat_task.start,all_non_repeat_task.end])
+			all_class_intervals.append([float(all_non_repeat_task.start.hour+float(all_non_repeat_task.start.minute)/60),float(all_non_repeat_task.end.hour+float(all_non_repeat_task.end.minute)/60)])
 
 	
 	# find the union of all intervals
