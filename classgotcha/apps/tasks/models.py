c55b983c2b4ee9761f7a87f8fb686e86023329b8
Yu-Tse Lin <yutse.lin0@gmail.com>
2017-08-30 20:30:16 -0400

    repeat_list optimized

--- classgotcha/apps/tasks/models.py
+++ classgotcha/apps/tasks/models.py
@@ -90,20 +90,4 @@ class Task(models.Model):
 
 	@property
 	def repeat_list(self):
-		repeat_list = []
-		if 'Mo' in self.repeat:
-			repeat_list.append(1)
-		if 'Tu' in self.repeat:
-			repeat_list.append(2)
-		if 'We' in self.repeat:
-			repeat_list.append(3)
-		if 'Th' in self.repeat:
-			repeat_list.append(4)
-		if 'Fr' in self.repeat:
-			repeat_list.append(5)
-		if 'Sa' in self.repeat:
-			repeat_list.append(6)
-		if 'Su' in self.repeat:
-			repeat_list.append(7)
-
-		return repeat_list
+		return [{'Mo':1, 'Tu':2, 'We':3, 'Th':4, 'Fr':5, 'Sa':6, 'Su':7}[x] for x in [self.repeat[i:i+2] for i in range(0, len(self.repeat), 2)]]
